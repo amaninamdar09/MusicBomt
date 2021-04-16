088847233296394a3b73faf0db30ce944f545e17
amaninamdar09 <79567211+amaninamdar09@users.noreply.github.com>
2021-04-16 11:23:02 +0530

    Update play.py
    Update Play.py

--- handlers/play.py
+++ handlers/play.py
@@ -19,7 +19,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @errors
 async def play(_, message: Message):
 
-    lel = await message.reply("🔄 **Processing** sounds...")
+    lel = await message.reply("🔄 **Processing** sabar kar thodasa...")
     sender_id = message.from_user.id
     sender_name = message.from_user.first_name
 
@@ -27,7 +27,7 @@ async def play(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        text="😎 Danish OP 😁🔥",
+                        text="🔥 DANISH OP 🔥",
                         url="https://t.me/idanishbaba")
                    
                 ]
@@ -40,7 +40,7 @@ async def play(_, message: Message):
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
             raise DurationLimitError(
-                f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed to play!"
+                f"❌ {DURATION_LIMIT} minute(s) se bada video..😮 mai nhi play karta ja..🙄"
             )
 
         file_name = get_file_name(audio)
@@ -51,7 +51,7 @@ async def play(_, message: Message):
     elif url:
         file_path = await converter.convert(youtube.download(url))
     else:
-        return await lel.edit_text("❗ You did not give me anything to play!")
+        return await lel.edit_text("❗ Are play karne kuch de to sahi..🙄")
 
     if message.chat.id in callsmusic.pytgcalls.active_calls:
         position = await queues.put(message.chat.id, file=file_path)
